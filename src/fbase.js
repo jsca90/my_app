87e2055f62af54df1d3009e4789240693eeba60a
jsca90 <78601997+jsca90@users.noreply.github.com>
2021-07-13 10:53:02 +0900

    make fbase.js

---
+++ src/fbase.js
@@ -0,0 +1,14 @@
+import { firebase } from "@firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+
+export const authService = firebase.auth();
+export const dbService = firebase.firestore();
+export const storageService = firebase.storage();
