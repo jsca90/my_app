6106555840f3690409c9a2aaeaf93719852fd049
jsca90 <mbc4805@gmail.com>
2021-07-27 07:42:26 +0900

    flex

--- src/routes/Data.js
+++ src/routes/Data.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import { dbService } from '../fbase';
+import React, { useState, useEffect } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import { dbService } from "../fbase";
 import { makeStyles } from "@material-ui/core/styles";
-
 
 const useData = () => {
     const [items, setItems] = useState([]);
@@ -18,42 +17,42 @@ const useData = () => {
                 }));
                 setItems([...item]);
             });
-
     }, []);
 
     return items;
 };
 
 const columns = [
-    { field: 'id', headerName: 'id', width: 90, hide: "ture", headerAlign: 'center', },
     {
-        field: 'ipaddr',
-        headerName: '아이피주소',
-        width: 200,
+        field: "id",
+        headerName: "id",
+        hide: "ture",
+        headerAlign: "center",
+        flex: 1,
+    },
+    {
+        field: "ipaddr",
+        headerName: "아이피주소",
         editable: false,
-        headerAlign: 'center',
+        headerAlign: "center",
+        flex: 1,
     },
     {
-        field: 'locationOfUse',
-        headerName: '사용 위치',
-        width: 200,
-        editable: false, headerAlign: 'center',
+        field: "locationOfUse",
+        headerName: "사용 위치",
+        editable: false,
+        headerAlign: "center",
+        flex: 1,
     },
     {
-        field: 'user',
-        headerName: '사용자명',
+        field: "user",
+        headerName: "사용자명",
         // type: 'number',
-        width: 150,
         editable: false,
-        headerAlign: 'center',
+        flex: 1,
+        headerAlign: "center",
     },
-
 ];
-
-
-
-
-
 
 export default function Data() {
     const rows = useData();
@@ -62,9 +61,10 @@ export default function Data() {
         setPageSize(params.pageSize);
     };
     return (
-        <div style={{
-            width: "100%"
-        }}>
+        <div
+            style={{
+                width: "100%",
+            }}>
             <DataGrid
                 rows={rows}
                 columns={columns}
@@ -73,8 +73,7 @@ export default function Data() {
                 rowsPerPageOptions={[10, 20, 30, 40, 50]}
                 pagination
                 autoHeight
-
             />
-        </div >
+        </div>
     );
 }
